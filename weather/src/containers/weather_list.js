cdebd94f58823305a863d09770762fc6159c4ced
Warodom Werapun <wwarodom@gmail.com>
2017-11-08 22:47:07 +0700

    Update weather with GoogleMap

--- weather/src/containers/weather_list.js
+++ weather/src/containers/weather_list.js
@@ -1,6 +1,7 @@
 import React, {Component} from 'react';
 import {connect} from 'react-redux';
 import Chart from '../components/chart';
+import GoogleMap from '../components/google_map';
 
 class WeatherList extends Component {
 
@@ -10,10 +11,11 @@ class WeatherList extends Component {
 		const temps = cityData.list.map( weather => weather.main.temp);
 		const pressure = cityData.list.map( weather => weather.main.pressure);
 		const humidity = cityData.list.map( weather => weather.main.humidity);
+ 		const {lon,lat} = cityData.city.coord;
 
 		return (
 			<tr key={name}>			    
-				<td>{name}</td>
+				<td><GoogleMap lon={lon} lat={lat} /></td>
 			    <td><Chart data={temps} color="orange" units="K" /></td> 	    			    
 			    <td><Chart data={pressure} color="red" units="hPa" /></td> 	 
 			    <td><Chart data={humidity} color="blue" units="%" /></td> 	 			    			    
